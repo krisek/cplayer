28c3fa2d33c1ac11a534304dc7e4b46b6219d4dc
Kristof Szabo <kris@lxsystems.de>
2022-09-01 07:52:07 +0200

    pwa initial seed

--- main.js
+++ main.js
@@ -23,3 +23,13 @@
       }
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", event => {
+    // we can move only if we are not in a browser's tab
+    isBrowser = matchMedia("(display-mode: browser)").matches;
+    if (!isBrowser) {
+       //window.moveTo(16, 16);
+       window.resizeTo(638, 636);
+    }
+ });
